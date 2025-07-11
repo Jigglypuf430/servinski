81e6fee192437efbe7b565211f1fe98b071ecc81
Jigglypuf430 <cbush2025@waverley.nsw.edu.au>
2025-07-11 09:34:57 +0000

    sdd

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // CONFIG – tune to taste
   const MAX_TILT_DEG = 25; // clamp device tilt we map
-  const MAX_SHIFT_PX = 40; // max pixel translation from centre
+  const MAX_SHIFT_PX = 0; // max pixel translation from centre
   const SPIN_SPEED = 0; // deg per frame (~9 deg/s @60fps)
 
   // ---------- DeviceOrientation (mobile) ----------
